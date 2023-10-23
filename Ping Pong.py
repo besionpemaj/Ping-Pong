87d7e21f80be34a352d31f3e0dc00ae007ae4258
Besion <148797756+besionpemaj@users.noreply.github.com>
2023-10-23 19:33:32 +0200

    Update Ping Pong.py
    Version 1.1

--- Ping Pong.py
+++ Ping Pong.py
@@ -32,21 +32,59 @@ class Player(GameSprite):
 
 win_width = 700
 win_height = 500
+
 window = display.set_mode((win_width,win_height))
 display.set_caption("Ping Pong")
 background=transform.scale(image.load("background.jpg"),(win_width,win_height))
+
 racket1=Player("racket.png",30,200,50,150,5)
 racket2=Player("racket.png",620,200,50,150,5)
 ball=Player("tenis_ball.png",300,300,50,50,5)
+
+font.init()
+font=font.Font(None,35)
+lose1=font.render("Player 1 lose",True,(255,0,0))
+lose2=font.render("Player 2 lose",True,(255,0,0))
+
+speed_x=3
+speed_y=3
 game=True
+finish=False
+
 while game:
 
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish !=True:
+       
+        window.blit(background,(0,0))
+        
+        racket1.update_right()
+        racket2.update_left()
+        
+        ball.rect.x+=speed_x
+        ball.rect.y+=speed_y
 
-    window.blit(background,(0,0))
-    racket1.reset()
-    racket2.reset()
-    ball.reset()
-    display.update()
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+            speed_x*=-1
+            speed_y*=1
+
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y*=-1
+            speed_x*=1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(250,300))
+            
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose2,(250,300))
+            
+        
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+        
+        display.update()
